3bc3f0b4cb0cfa4bce1bb5aa71432ba8fd076b65
Meral Özarslan Yatak <meralozarslan@gmail.com>
2018-03-21 20:28:24 +0300

    harika oldu.

---
+++ alistirma/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int sayi1;
+    int sayi2;
+    int sonuc;
+    char islem;
+    printf("İlk sayıyı giriniz:%d\n",sayi1);
+    //scanf("%d",&sayi);
+    printf("İşlemi seçiniz; toplama(+) veya t/T, cıkarma (-) vea c/C, bölme (/) veya b/B, çarpma(*) veya r/R :%d\n",islem);
+    printf("İkinci sayıyı giriniz:%d\n",sayi2);
+   /* switch (islem){
+            case "+" or "t/T":
+            sonuc=sayi1+sayi2;
+            break
+            case "-" or "c/C":
+        */
+
+
+
+
+    return 0;
+}
+
